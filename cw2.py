09fe0745eb395ea31a66dcae0e8aced6c222ec63
Alex Martin <alexmartin153@live.co.uk>
2023-03-23 17:51:17 +0000

    first commenting pass

--- cw2.py
+++ cw2.py
@@ -12,36 +12,80 @@ from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 
 class Recommender:
     def __init__(self, recipes_path):
+        """
+            Initialize the Recommender class with a path to the recipes dataset.
+
+            Args:
+                recipes_path (str): A string representing the path to a CSV file containing recipe data.
+                    The file should contain a header row with column names and the first column should be
+                    an index or ID column. If any values in the 'category' or 'cuisine' columns are missing,
+                    they will be filled with the string 'unknown'. Any other missing values in the dataset should
+                    be represented as NaN.
+        """
+
+        # Load the dataset into a pandas DataFrame
         self.df = pd.read_csv(recipes_path, na_values=['', ' '])
+
+        # Fill any missing values in the 'category' and 'cuisine' columns with 'unknown'
         self.df.fillna({'category': 'unknown', 'cuisine': 'unknown'}, inplace=True)
+
+        # Drop the 'image_url' and 'recipe_url' columns, which are not needed for recommendation
         self.df = self.df.drop(['image_url', 'recipe_url'], axis=1)
+
+        # Drop the first column of the dataset, which is assumed to be an index or ID column
         self.df = self.df.drop(self.df.columns[0], axis=1)
+
+        # Build a K-Nearest Neighbors model based on the recipe dataset
         self._build_knn_model()
 
     # Part 1 Task 1
     def stats(self):
+        """
+            Return summary statistics for the recipe dataset.
+
+            Returns:
+                pd.DataFrame: A pandas DataFrame containing summary statistics for the recipe dataset.
+        """
         return self.df.describe()
 
     # Part 1 Task 1
     def top(self):
+        """
+            Return the top 10 recipes with the highest average rating.
+
+            Returns:
+                pd.DataFrame: A pandas DataFrame containing the top 10 recipes with the highest average rating,
+                including the 'title' and 'rating_avg' columns.
+        """
         top10 = self.df.nlargest(10, 'rating_avg')
         return top10[['title', 'rating_avg']]
 
     # Part 1 Task 2
     def rating_vs_num_ratings(self):
+        """
+            Generate scatter plots of average rating vs. number of ratings for all recipes,
+            and for recipes with and without significant ratings.
+
+            Returns:
+                str: A message indicating that the graphs have been generated.
+        """
+
+        # Generate a scatter plot of average rating vs. number of ratings for all recipes
         plt.scatter(self.df['rating_val'], self.df['rating_avg'])
         plt.xlabel('Number of Ratings')
         plt.ylabel('Average Rating')
         plt.title('Relationship between Average Rating and Number of Ratings')
         plt.show()
 
-        threshold = 100  # Define a threshold for the number of ratings
+        # Define a threshold for the number of ratings
+        threshold = 100
 
         # Filter the recipes based on the number of ratings threshold
         significant_ratings = self.df[self.df['rating_val'] >= threshold]
         not_significant_ratings = self.df[self.df['rating_val'] < threshold]
 
-        # Plot the significant and not significant ratings separately
+        # Generate a scatter plot of average rating vs. number of ratings
+        # for recipes with and without significant ratings
         plt.scatter(significant_ratings['rating_val'], significant_ratings['rating_avg'], label='Significant Ratings')
         plt.scatter(not_significant_ratings['rating_val'], not_significant_ratings['rating_avg'],
                     label='Not Significant Ratings')
@@ -51,14 +95,31 @@ class Recommender:
         plt.legend()
         plt.show()
 
+        # Return a message indicating that the graphs have been generated
         return f"Graphs have been generated"
 
     # Part 1 Task 3
     def recommended_recipes(self, recipe_title):
+        """
+            Generate recommendations for a given recipe title using cosine similarity.
+
+            Args:
+                recipe_title (str): The title of the recipe for which to generate recommendations.
+
+            Returns:
+                str: A string containing the titles of the 10 most similar recipes to the input recipe,
+                in descending order of similarity.
+                """
+        # Make a copy of the dataset
         df = self.df.copy()
+
+        # Define the number of recommended recipes to return
         num_recommendations = 10
+
+        # Define the features to use for similarity calculation
         features = ['title', 'rating_avg', 'rating_val', 'total_time', 'category', 'cuisine', 'ingredients']
 
+        # Combine the selected features into a single text feature
         df['combine_features'] = df[features].apply(lambda x: ' '.join(x.astype(str)), axis=1)
 
         # Use CountVectorizer to convert the text data into a matrix of token counts
@@ -68,6 +129,7 @@ class Recommender:
         # Compute the cosine similarity matrix
         similarity_matrix = cosine_similarity(features_matrix)
 
+        # Find the index of the recipe with the given title
         recipe_index = df[df['title'] == recipe_title].index[0]
 
         # Get the indices of the most similar recipes
@@ -75,12 +137,26 @@ class Recommender:
 
         # Get the titles of the most similar recipes
         recommended_recipes = df.iloc[similar_indices]['title'].to_string()
+
+        # Return the recommended recipe titles as a string
         return recommended_recipes
 
     # Part 2 Task 1
     def vec_space_method(self, recipe_title):
-        # Preprocess the dataset
+        """
+            Generate recommendations for a given recipe title using the vector space method.
+
+            Args:
+                recipe_title (str): The title of the recipe for which to generate recommendations.
+
+            Returns:
+                pd.Series: A pandas Series containing the titles of the 10 most similar recipes to the input recipe,
+                in descending order of similarity.
+        """
+
+        # Make a copy of the dataset
         df = self.df.copy()
+
         # One-hot encode categorical features
         categorical_features = ['category', 'cuisine', 'ingredients']
         one_hot_encoder = OneHotEncoder(handle_unknown='ignore')
@@ -108,6 +184,17 @@ class Recommender:
 
     # Part 2 Task 2
     def _build_knn_model(self):
+        """
+            Build a KNN model for recipe recommendations.
+
+            This method uses the KNN algorithm to find the k most similar recipes to a given recipe.
+
+            The model is built by vectorising the recipe titles using TF-IDF, scaling numerical features,
+            one-hot encoding categorical features, and combining all features into a single matrix.
+
+            The KNN model is trained on the combined feature matrix, using cosine similarity as the distance metric.
+        """
+
         # Vectorize recipe titles using TF-IDF
         self.vectoriser = TfidfVectorizer(stop_words='english', ngram_range=(1, 2))
         title_matrix = self.vectoriser.fit_transform(self.df['title'])
@@ -118,9 +205,11 @@ class Recommender:
         scaled_numerical_matrix = self.scaler.fit_transform(self.df[numerical_features])
         scaled_numerical_df = pd.DataFrame(scaled_numerical_matrix, columns=numerical_features)
 
-        # Combine categorical and numerical features
+        # One-hot encode categorical features
         self.category_df = pd.get_dummies(self.df['category'])
         self.cuisine_df = pd.get_dummies(self.df['cuisine'])
+
+        # Combine all features into a single matrix
         combined_matrix = pd.concat(
             [pd.DataFrame(title_matrix.toarray(), columns=self.vectoriser.get_feature_names_out()), scaled_numerical_df,
              self.category_df, self.cuisine_df], axis=1)
@@ -134,11 +223,22 @@ class Recommender:
 
     # Part 2 Task 2
     def knn_similarity(self, recipe_title):
+        """
+        Returns a pandas Series containing the titles of the 10 most similar recipes to the input recipe.
+
+        Args:
+            recipe_title (str): The title of the recipe for which to generate recommendations.
+
+        Returns:
+            pd.Series: A pandas Series containing the titles of the 10 most similar recipes to the input recipe,
+            excluding the input recipe title.
+        """
+
         # Transform the title using the fitted vectorizer
         title_vector = self.vectoriser.transform([recipe_title])
 
         # Get numerical and categorical features in one pass
-        input_row = self.df.query("title == @title")
+        input_row = self.df.query("title == @recipe_title")
 
         # Scale numerical features
         numerical_features = ['rating_avg', 'rating_val', 'total_time']
@@ -168,49 +268,77 @@ class Recommender:
         # Get 10 most similar recipe indices and distances
         distances, indices = self.knn_model.kneighbors(input_vector, return_distance=True)
 
-        # Return the most similar recipe titles, excluding the first one (the same title)
+        # Return the most similar recipe titles, excluding the input recipe title
         return self.df.iloc[indices[0][1:]]['title']
 
     # Part 2 Task 3
     def calculate_metrics(self, recommendations):
+        """
+        Calculates the coverage and personalisation metrics for a set of recommendations.
+
+        Args:
+            recommendations (Dict[int, List[str]]): A dictionary mapping user IDs to lists of recommended recipe titles.
+
+        Returns:
+            Tuple[float, float]: A tuple containing the coverage and personalisation metrics as floats.
+        """
+
         all_recommendations = set()
         total_items = self.df.shape[0]
         unique_recommendations = 0
         cosine_similarities = []
 
+        # Calculate the number of unique recommended items and the total number of recommended items
         for user_recommendations in recommendations.values():
             all_recommendations.update(user_recommendations)
             unique_recommendations += len(set(user_recommendations))
 
+        # Calculate the coverage metric
         coverage = len(all_recommendations) / total_items
 
         recommendation_vectors = []
         for recs in recommendations.values():
+            # Create a binary vector of recommended items for each user
             rec_vector = [1 if r in recs else 0 for r in all_recommendations]
             recommendation_vectors.append(rec_vector)
 
+        # Calculate the cosine similarities between the user vectors
         cosine_similarities_matrix = cosine_similarity(recommendation_vectors)
 
         for i in range(cosine_similarities_matrix.shape[0]):
             for j in range(i + 1, cosine_similarities_matrix.shape[1]):
                 cosine_similarities.append(cosine_similarities_matrix[i, j])
 
+        # Calculate the personalisation metric
         personalisation = 1 - np.mean(cosine_similarities)
 
         return coverage, personalisation
 
     # Part 2 Task 3
     def evaluate_recommenders(self, test_set):
+        """
+        Evaluates the KNN and vector space recommenders on a test set of user-liked recipes.
+
+        Args:
+            test_set (Dict[int, str]): A dictionary mapping user IDs to recipe titles that they liked.
+
+        Returns:
+            str: A string summarizing the evaluation results for the KNN and vector space recommenders.
+        """
+
         knn_recommendations = {}
         vec_space_recommendations = {}
 
+        # Generate recommendations for each user in the test set using both recommenders
         for user, liked_recipe in test_set.items():
             knn_recommendations[user] = self.knn_similarity(liked_recipe).tolist()
             vec_space_recommendations[user] = self.vec_space_method(liked_recipe).tolist()
 
+        # Calculate the coverage and personalisation metrics for both recommenders
         knn_coverage, knn_personalisation = self.calculate_metrics(knn_recommendations)
         vec_space_coverage, vec_space_personalisation = self.calculate_metrics(vec_space_recommendations)
 
+        # Format the evaluation results into a string
         result = f"Based on a test set of {len(test_set)} users:\n"
         result += f"KNN Recommender: Coverage = {knn_coverage:.3f}," \
                   f" Personalisation = {knn_personalisation:.2f}\n"
@@ -219,6 +347,18 @@ class Recommender:
         return result
 
     def tasty_check(self, recipe_title):
+        """
+            Checks if a given recipe is "tasty" (i.e., has an average rating above 4.2)
+             using a logistic regression model.
+
+            Args:
+                recipe_title (str): The title of the recipe to check.
+
+            Returns:
+                str: A string indicating whether the input recipe is tasty or not,
+                 and the accuracy of the logistic regression model.
+        """
+
         # Transform the rating_avg column into binary format
         self.df['tasty'] = np.where(self.df['rating_avg'] > 4.2, 1, -1)
 
@@ -257,37 +397,60 @@ class Recommender:
 
 
 class Driver:
-    def __init__(self, recipes):
-        self.recipes = recipes
+    def __init__(self, recipes_path):
+        """
+        Constructor for the Driver class.
+        :param recipes_path: The path to the CSV file containing recipe data.
+        """
+        self.recipes = recipes_path
         self.df = Recommender(self.recipes)
 
     def task1(self):
+        """
+        Print statistics for the recipe dataset and the top 10 highest rated recipes.
+        """
         task1 = self.df
         print("=" * 50, "Task 1", "=" * 50)
         print(task1.stats())
         print(task1.top())
 
     def task2(self):
+        """
+        Plot the relationship between average rating and number of ratings for all recipes, and
+        separately for recipes with a number of ratings greater than or equal to 100.
+        """
         task2 = self.df
         print("=" * 50, "Task 2", "=" * 50)
         print(task2.rating_vs_num_ratings())
 
     def task3(self):
+        """
+        Print the top 10 recommended recipes based on cosine similarity with the input recipe.
+        """
         task3 = self.df
         print("=" * 50, "Task 3", "=" * 50)
         print(task3.recommended_recipes("English muffins"))
 
     def task4(self):
+        """
+        Print the top 10 recommended recipes based on the vector space model method with the input recipe.
+        """
         task4 = self.df
         print("=" * 50, "Task 4", "=" * 50)
         print(task4.vec_space_method("Chicken and coconut curry"))
 
     def task5(self):
+        """
+        Print the titles of the 10 most similar recipes to the input recipe based on the KNN method.
+        """
         task5 = self.df
         print("=" * 50, "Task 5", "=" * 50)
         print(task5.knn_similarity("Chicken and coconut curry").to_string())
 
     def task6(self):
+        """
+        Evaluate the KNN and Vector Space Recommender systems on a test set of 4 users.
+        """
         task6 = self.df
         print("=" * 50, "Task 6", "=" * 50)
         test_set = {
@@ -304,6 +467,9 @@ class Driver:
         print(task6.evaluate_recommenders(test_set))
 
     def task7(self):
+        """
+        Check the tastiness of a given recipe.
+        """
         task7 = self.df
         print("=" * 50, "Task 7", "=" * 50)
         print(task7.tasty_check("Chicken tikka and naan bread"))
